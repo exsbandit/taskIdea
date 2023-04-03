a1f7d93d608533a6a32a64a0e3a7119f341d8a80
exsbandit <ates_erdem@hotmail.com>
2023-04-03 19:42:27 +0300

    File paths changed

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\v1;
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('/order')
+    ->controller(v1\OrderController::class)
+    ->group(function () {
+        Route::get('/products', 'orderedProducts');
+        Route::get('/discounts', 'orderedDiscounts');
+    });
+
+Route::apiResource('category', v1\CategoryController::class);
+Route::apiResource('customer', v1\CustomerController::class);
+Route::apiResource('order', v1\OrderController::class);
+Route::apiResource('product', v1\ProductController::class);
+
